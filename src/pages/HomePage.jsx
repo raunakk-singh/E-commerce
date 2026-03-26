47da0c9ce71b99b952667c7bb29f7110f5978c60
Raunak Singh <raunaksinghyt2@gmail.com>
2026-03-26 23:00:28 +0530

    get cart from backend and display

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,11 +7,16 @@ import './HomePage.css';
 
 export function HomePage() {
   const [products,setProducts]=useState([]);
+  const [cart,setCart]=useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/api/products")
       .then((response) => {
         setProducts(response.data);
       });
+      axios.get('http://localhost:3000/api/cart-items')
+      .then((response)=>{
+        setCart(response.data);
+      })
 
   },[]);
 
@@ -19,7 +24,7 @@ export function HomePage() {
     <>
       <title>E-commerce</title>
 
-      <Header />
+      <Header cart={cart}/>
 
 
       <div className="home-page">
