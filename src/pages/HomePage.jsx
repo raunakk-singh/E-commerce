44f957a757c9f2761067f545c05be8981a4bcfcb
Raunak Singh <raunaksinghyt2@gmail.com>
2026-03-26 16:36:52 +0530

    Move checkout page in react

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import './HomePage.css';
 export function HomePage(){
 return(
   <>
+    <title>E-commerce</title>
         <div className="header">
           <div className="left-section">
             <a href="index.html" className="header-link">
