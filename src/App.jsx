b9bfef953e4005ce4ff7c22abcbb21598f6590fc
Raunak Singh <raunaksinghyt2@gmail.com>
2026-03-27 19:22:50 +0530

    lift cart up and shred between pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import OrdersPage from './pages/OrdersPage';
-import {Tracking} from './pages/Tracking';
+import { Tracking } from './pages/Tracking';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css'
+import { use } from 'react';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
 
+  }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />}></Route>
-      <Route path="tracking" element={<Tracking/>}></Route>
+      <Route path="tracking" element={<Tracking />}></Route>
     </Routes>
 
 
